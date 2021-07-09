1c70984276134550cdbe017513fdf56a5e9b4b02
Mike Pluta <mike.pluta@gmail.com>
2021-07-08 22:05:51 -0500

    update with level set functionality

--- devicetypes/alexa/alexa-dimmer-switch.src/alexa-dimmer-switch.groovy
+++ devicetypes/alexa/alexa-dimmer-switch.src/alexa-dimmer-switch.groovy
@@ -15,9 +15,11 @@ metadata {
 
     definition (name: "Alexa Dimmer Switch", namespace: "alexa", author: "mikepluta") {
         capability "Switch"
+        capability "Switch Level"
         capability "Sensor"
+        capability "Contact Sensor"
         capability "Actuator"
-        capability "Contact Sensor"	    		
+        capability "Refresh"
     }
     
     simulator {
@@ -25,25 +27,85 @@ metadata {
 		status "closed": "contact:closed"
 	}
 
-    tiles {
-        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-            state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-            state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC"
+    // tiles {
+    //     standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+    //         state "off", label: '${currentValue}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+    //         state "on", label: '${currentValue}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC"
+    //     }
+    //     main "switch"
+    //     details(["switch"])
+    // }
+    
+    // from dimmer....
+    tiles(scale: 2) {
+		multiAttributeTile(name: "switch", type: "switch", width: 6, height: 4, canChangeIcon: true, canChangeBackground: true) {
+			tileAttribute("device.switch", key: "PRIMARY_CONTROL") {
+    			attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
+		      	attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "turningOff"
+				attributeState "turningOff", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "turningOn"
+		      	attributeState "turningOn", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "turningOff"
+        	}
+        	tileAttribute("device.level", key: "SLIDER_CONTROL") {
+            	attributeState "level", action:"switch level.setLevel"
+        	}
+      		tileAttribute("level", key: "SECONDARY_CONTROL") {
+            	attributeState "level", label: 'Switch Level set to ${currentValue}'
+        	}
+		}
+        
+        valueTile("lValue", "device.level", inactiveLabel: true, height:2, width:2, decoration: "flat") {  
+			state "levelValue", label:'${currentValue}', unit:"", backgroundColor: "#53a7c0"  
         }
-        main "switch"
-        details(["switch"])
-    }
+        
+		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+        
+		main "switch"
+		details(["switch","lValue","refresh"])
+
+	}
+    
 }
 
-def parse(description) {
+def parse(String description) {
 }
 
 def on() {
+	log.info "switch:on, contact:open"
     sendEvent(name: "switch", value: "on")
     sendEvent(name: "contact", value: "open")
 }
 
 def off() {
+	log.info "switch:off, contact:closed"
     sendEvent(name: "switch", value: "off")
     sendEvent(name: "contact", value: "closed")
+}
+
+def setLevel(val) {
+    log.info "setLevel $val"
+    
+    // make sure we don't drive switches past allowed values (command will hang device waiting for it to
+    // execute and never come back)
+    if (val < 0) {
+    	val = 0
+    }
+
+    if (val > 100) {
+    	val = 100
+    }
+
+    if (val == 0) { 
+    	sendEvent(name: "level", value: val)
+    	off()
+    } else {
+    	on()
+    	sendEvent(name: "level", value: val)
+    	sendEvent(name: "switch.setLevel", value: val)
+    }
+}
+
+def refresh() {
+    log.info "refresh"
 }
